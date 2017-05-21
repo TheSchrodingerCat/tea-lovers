1d3a95faa4639f83b12d7238b7fc1eed3eaafb3e
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-20 23:08:08 -0600

    agregada las validaciones, alertas y arreglos en css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
 
-function principal(){
+document.getElementById("suscribirse").addEventListener("click",validacion);
+
+function validacion(){
 	
+	var name = document.getElementById("name").value;
+	var mail = document.getElementById("email").value;
+	var dir = document.getElementById("dir").value;
+	var eleccion = document.getElementsByTagName("select");
+
+	var validateName = (/^[A-Z][a-z]{3,19}\s[A-Z][a-z]{3,19}$/).test(name);
+	var validateMail = (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(mail);
+	var validateDir = (/^[A-Z][a-z]{3,19}\s[0-9]{1,6}$/).test(dir);
+	var validateSelect = function(){
+		var large = eleccion.length;
+		for (i=0 ; i<large ; i++){
+			if (eleccion[i].value == "Selecciona"){
+				return true;
+			}
+		}
+	}
+
+
+	if (!validateName || !validateMail || !validateDir || validateSelect()){
+		alert("¡ERROR! Revisa una vez más los datos ingresados.");
+	} else {
+		alert("¡Muchas gracias por suscribirte!");
+	}
+
 }
